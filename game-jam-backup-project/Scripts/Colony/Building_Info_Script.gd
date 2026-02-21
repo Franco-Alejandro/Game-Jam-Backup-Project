b6bedaddcb3bf1a48b7d299e96e0a61beee19657
Joakim Ståhle-Nilsson <joakimthegamer@yahoo.se>
2026-02-21 18:08:17 +0100

    Made it so that you can buy buildings

--- game-jam-backup-project/Scripts/Colony/Building_Info_Script.gd
+++ game-jam-backup-project/Scripts/Colony/Building_Info_Script.gd
@@ -9,9 +9,12 @@ func set_building(associated_building: Building) -> void:
 	label.text = ""
 	var resource: BuildingResource = building.building_resource
 	
-	for key in resource.required_resources:
-		label.text += key + ": " + str(resource.required_resources[key]) + "\n"
+	label.text += resource.building_name + "\n\n"
+	label.text += resource.description + "\n\n"
+	
+	for key: ResourceType.RESOURCE_ID in resource.required_resources:
+		label.text += ResourceType.RESOURCE_ID.keys()[key] + ": " + str(resource.required_resources[key]) + "\n"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
