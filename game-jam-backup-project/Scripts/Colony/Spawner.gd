7c88e9a6797a9d481b88a9119616d19423919bf9
Franco Amatta <franco.alejandro.a@gmail.com>
2026-02-22 00:19:39 +0100

    Randomizing names

--- game-jam-backup-project/Scripts/Colony/Spawner.gd
+++ game-jam-backup-project/Scripts/Colony/Spawner.gd
@@ -4,7 +4,9 @@ class_name Spawner
 @export var penguin_scene: PackedScene
 @export var spawn_interval: float = 5.0
 @export var spawn_radius: float = 10.0
+@export var names_database: PenguinNames  
 
+var used_names: Array[String] = []
 var _spawned_penguins: Array = []
 
 func _ready():
@@ -28,7 +30,8 @@ func spawn_penguin():
 	get_parent().add_child(penguin)
 	
 	penguin.global_position = global_position
-	
+	penguin.penguin_data.penguin_name = get_unique_name()
+
 	var random_offset = Vector3(
 		randf_range(-spawn_radius, spawn_radius),
 		0,
@@ -43,5 +46,27 @@ func spawn_penguin():
 	_spawned_penguins.append(penguin)
 	
 func can_spawn() -> bool:
-	# TODO: Add real resource logic later
+	# TODO: Add real resource logic later fish?
 	return true
+
+func get_unique_name() -> String:
+	var available_names = get_available_names()
+	if available_names.size() > 0:
+		var random_index = randi() % available_names.size()
+		var new_name = available_names[random_index]
+		used_names.append(new_name)
+		return new_name
+	else:
+		# we made too many, add a number
+		return "Penguin " + str(_spawned_penguins.size() + 1)
+
+func get_available_names() -> Array[String]:
+	var all_names: Array[String] = []
+	if names_database:
+		for first in names_database.first_names:
+			for last in names_database.last_names:
+				var full_name = first + " " + last
+				if full_name not in used_names:
+					all_names.append(full_name)
+	
+	return all_names
---
+++ game-jam-backup-project/Scripts/Data/Penguin_Names.gd
@@ -0,0 +1,14 @@
+extends Resource
+class_name PenguinNames
+
+@export var first_names: Array[String] = [
+	"Peppy", "Juan", "Chilly", "Flippers", "Snowball",
+	"Pippin", "Gunter", "Kowalski", "Rico", "Private",
+	"Skipper", "Dart", "Merry", "Tux", "Pingu", "Joaquin", "Maddy",
+	"Felice", "Stefano", "Andres", "Frenk", "Vincenzo"
+]
+
+@export var last_names: Array[String] = [
+	"Portis", "Skaner", "Swede", "Malma", "Copen",
+	"Denark", "Tartaro", "Glacier", "Snowball", "Penguinson"
+]
