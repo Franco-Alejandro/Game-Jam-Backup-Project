d61bd7b8a0b57299dfca2c6ad25da20e64cdc2db
Franco-Alejandro <franco.alejandro.a@gmail.com>
2026-02-22 00:07:51 +0100

    Added cozyness cost and regen (#14)

--- game-jam-backup-project/Scripts/Data/Penguin_Task.gd
+++ game-jam-backup-project/Scripts/Data/Penguin_Task.gd
@@ -8,6 +8,7 @@ enum TASK_TYPE { GATHER_ICE_CREAM, GATHER_PEBBLES, GATHER_FISH, PLAY_AROUND}
 @export var description: String
 @export var duration: float
 @export var cozyness_cost: int
+@export var cozyness_reward: int = 0  
 @export var rewards: Dictionary[ResourceType.RESOURCE_ID, int] = {
 	ResourceType.RESOURCE_ID.FISH: 0,
 	ResourceType.RESOURCE_ID.ICE_CREAM: 0,
--- game-jam-backup-project/Scripts/Entities/Penguin_Brain.gd
+++ game-jam-backup-project/Scripts/Entities/Penguin_Brain.gd
@@ -1,11 +1,12 @@
 extends CharacterBody3D
 class_name PenguinBrain
 
+@export var play_task_resource: TaskResource = preload("res://Resources/Tasks/play_task.tres")
+
 enum PenguinState { IDLE, SELECT_LOCATION, RUNNING_TO_TASK, DOING_TASK }
 var _current_state : PenguinState = PenguinState.IDLE;
 
 var colony : Colony;
-static var stopping_distance: float = 0.5
 
 static var task_type_to_anim_flag: Dictionary[TaskResource.TASK_TYPE, String] = {
 	TaskResource.TASK_TYPE.GATHER_ICE_CREAM: "parameters/conditions/is_task_icecream",
@@ -21,6 +22,11 @@ var behaviour : Dictionary = {
 var task_duration_left : float = 0
 var penguin_data : PenguinResource = PenguinResource.new() 
 var target_building : Building;
+
+#Cozyness
+var cozyness_regen_timer: float = 0
+@export var cozyness_regen_interval: float = 5.0  
+@export var cozyness_regen_amount: int = 1
 
 # DEBUG STUFF
 @onready var nav = $NavigationAgent3D
@@ -40,12 +46,24 @@ func find_animation_tree(node: Node) -> AnimationTree:
 	
 func _ready():
 	penguin_data.penguin_name = "Juan"
+	penguin_data.cozyness = 3
 	add_to_group("penguins")
 	anim_tree = find_animation_tree(self)
 	
 func _process(delta: float) -> void:
+	cozyness_regen_timer += delta
+	if cozyness_regen_timer >= cozyness_regen_interval:
+		regenerate_cozyness()
+		cozyness_regen_timer = 0
+		
 	if behaviour.get(_current_state) != null:
 		behaviour[_current_state].call(delta)
+
+func regenerate_cozyness():
+	if penguin_data:
+		var new_cozyness = min(penguin_data.cozyness + cozyness_regen_amount, penguin_data.max_cozyness)
+		if new_cozyness != penguin_data.cozyness:
+			penguin_data.cozyness = new_cozyness
 	
 func _physics_process(delta):
 	# Gravity
@@ -71,7 +89,6 @@ func _physics_process(delta):
 	direction.y = 0
 	
 	if direction.length() > 0.1:
-		# Rotation
 		look_at(global_position - direction, Vector3.UP)
 		
 		direction = direction.normalized()
@@ -94,12 +111,10 @@ func set_state(new_state: PenguinState):
 	reset_animation_state()
 	_current_state = new_state
 	update_animation_state()
-	print_animation_flags()
 	
 func reset_animation_state():
 	anim_tree["parameters/conditions/is_moving"] = false
 	anim_tree["parameters/conditions/is_idle"] = false
-	# Turn all task flags off
 	for flag_path in task_type_to_anim_flag.values():
 		anim_tree[flag_path] = false
 	
@@ -124,14 +139,20 @@ func is_building_accesible(building: Building) -> bool:
 	return true
 
 func set_task(task: TaskResource):
-	var bebe = get_tree().get_nodes_in_group("Colony")
+	# can't pay for the task
+	if penguin_data.cozyness <= task.cozyness_cost:
+		force_play_task()
+		return
+		
 	colony = get_tree().get_nodes_in_group("Colony").pick_random()
 	
 	if !colony:
 		return;
+		
 	#ignore same tasks
 	if penguin_data.current_task and penguin_data.current_task == task:
 		return
+		
 	penguin_data.current_task = task
 	
 	var available_buildings : Array[Building] = colony.get_built_buildings();
@@ -150,12 +171,22 @@ func set_task(task: TaskResource):
 	task_duration_left = task.duration
 
 func idle(_delta: float):
-	pass
+	if penguin_data.cozyness < penguin_data.max_cozyness * 0.3:  # Below 30%
+		force_play_task()
 	
-func select_location(delta: float):
+func select_location(_delta: float):
 	if penguin_data.current_task == null:
 		set_state(PenguinState.IDLE)
 		return;
+	
+	 #play tasks just pick a random nearby location instead of going to a building
+	if penguin_data.current_task.task_type == TaskResource.TASK_TYPE.PLAY_AROUND:
+		var random_direction = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
+		var random_distance = randf_range(5, 15)
+		var play_location = global_position + random_direction * random_distance
+		target_position(play_location)
+		set_state(PenguinState.RUNNING_TO_TASK)
+		return
 	
 	if target_building == null:
 		return;
@@ -169,32 +200,47 @@ func do_task(delta: float):
 	
 	if int(task_duration_left + delta) > int(task_duration_left):
 		print("Penguin is doing ", penguin_data.current_task.task_name)
-		print("time left ", int(task_duration_left), " of ", penguin_data.current_task.duration)
 	
 	if (task_duration_left <= 0):
 		finish_task()
 		
 
-func move_to(target_position : Vector3):
-	target_position(target_position);
+func move_to(position_to_go_to : Vector3):
+	target_position(position_to_go_to);
 	set_state(PenguinState.RUNNING_TO_TASK)
 	
 
 func finish_task():
 	if penguin_data.current_task:
-		var resource_manager := ResourceManagerSingleton
-		if resource_manager:
-			for resource_id in penguin_data.current_task.rewards:
-				var amount: int = penguin_data.current_task.rewards[resource_id]
-				resource_manager.add_resource(resource_id, amount)
+		var task = penguin_data.current_task
+		if task.task_type == TaskResource.TASK_TYPE.PLAY_AROUND:
+			penguin_data.cozyness += task.cozyness_reward
+			penguin_data.cozyness = min(penguin_data.cozyness, penguin_data.max_cozyness)
+		else:
+			var resource_manager := ResourceManagerSingleton
+			if resource_manager:
+				for resource_id in task.rewards:
+					var amount: int = task.rewards[resource_id]
+					resource_manager.add_resource(resource_id, amount)
+		penguin_data.cozyness -= penguin_data.current_task.cozyness_cost
 		penguin_data.current_task = null
 	task_duration_left = 0
 	if target_building:
 		target_building.is_being_used = false
 		target_building = null
+
 	set_state(PenguinState.IDLE)
-	
-	
+
+func force_play_task():
+	if play_task_resource:
+		penguin_data.current_task = play_task_resource
+		task_duration_left = play_task_resource.duration
+		target_building = null
+		set_state(PenguinState.SELECT_LOCATION)
+		print("Penguin is too tired! Forced to play to regain cozyness.")
+	else:
+		print("No play task available!")
+		
 func print_animation_flags():
 	print("---- Animation Flags ----")
 	print("is_moving: ", anim_tree["parameters/conditions/is_moving"])
